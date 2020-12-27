d06ae39d90942c977b395ecdd22446d14d45c527
sarathi@idsoftsource.com <sarathi@idsoftsource.com>
2020-12-27 15:13:34 +0530

    site added

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutes } from './dashboard.routing';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NavigationModule } from '../navigation/navigation.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRoutes
+    DashboardRoutes,
+    NavigationModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, ProjectsComponent]
 })
 export class DashboardModule { }
